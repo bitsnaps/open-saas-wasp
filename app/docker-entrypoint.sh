b6f8ad2c45fdebb90f5f94977389965f866bb431
Adryan Serage <17680194+KazeroG@users.noreply.github.com>
2024-02-05 02:15:58 -0500

    Create docker-entrypoint.sh
    Signed-off-by: Adryan Serage <17680194+KazeroG@users.noreply.github.com>

---
+++ app/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Run database migrations
+wasp db migrate-dev
+
+# Optionally start the Wasp development server
+# Note: You might want to handle starting the server differently depending on your setup
+# For example, you could use CMD ["wasp", "start"] in the Dockerfile to start the server and omit from here
+
+# Keep the container running if you're not starting the Wasp server directly
+# This could be useful if you're using `docker-compose up` to manage services together
+# tail -f /dev/null
+
+# If you're directly starting Wasp's server and potentially Wasp DB studio,
+# consider the implications on container lifecycle and logging
+wasp start
+
+# To use Wasp DB studio, you'd typically run it in a development environment
+wasp db studio
+
+# Execute the command provided to the docker run command, if any
+exec "$@"
